c50797416149c41c9107803b4791c6ca92c55714
Agrim Gupta <agrimgupt195@gmail.com>
2025-04-03 20:31:26 +0530

    update

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const app = express();
 const port = 3000;
 
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, "/")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "index.html"));
+});
 app.get('/take-screenshot', async (req, res) => {
   try {
     const browser = await puppeteer.launch({ headless: "new" });
